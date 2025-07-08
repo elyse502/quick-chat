0b353981825427e8d743dccb766c8d94e2e9469c
elyse502 <elyseniyibizi502@gmail.com>
2025-07-08 18:20:39 +0200

    feat: Add size to home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <h1 className="text-white">Home Page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
