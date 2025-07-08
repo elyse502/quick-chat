94899ca152a44a4e75853d0d3acb79b3e4527215
elyse502 <elyseniyibizi502@gmail.com>
2025-07-08 18:09:13 +0200

    feat: Add global background image to application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
-    <div className="bg-gray-500">
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
