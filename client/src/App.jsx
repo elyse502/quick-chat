7e462f88dc6de8a4a90552eff1923f1f2255163d
elyse502 <elyseniyibizi502@gmail.com>
2025-07-08 17:33:23 +0200

    feat: Add support for Google Outfit font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      
+      <h1 className="text-3xl text-indigo-700">MyApp</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
