e4c60e9a2aed2b6e5eb953828f0a171b3936f731
elyse502 <elyseniyibizi502@gmail.com>
2025-07-29 20:14:38 +0200

    feat(auth): Implement user login with credential validation and JWT generation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,3 +41,29 @@ export const signup = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Controller to login a user
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await User.findOne({ email });
+
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+    if (!isPasswordCorrect) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = generateToken(newUser._id);
+
+    return res.json({
+      success: true,
+      userData,
+      token,
+      message: "Login successful",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
