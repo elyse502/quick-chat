1506dda42ac81e332fb263b573cebf3d1080bfc2
elyse502 <elyseniyibizi502@gmail.com>
2025-07-30 11:55:01 +0200

    feat(auth): Set up AuthContext and wrap app with AuthProvider

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+import axios from "axios";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const value = {
+    axios,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
